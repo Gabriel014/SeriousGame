7802364ad10cfeb7f946631480052414e5f9998a
BrazilGabriel <gabriel.br1994@gmail.com>
2016-05-22 08:45:44 -0400

    Menus e botões
    Basico do menu e interação entre cenas

---
+++ Assets/Scripts/ButtonsController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonsController : MonoBehaviour {
+
+	public void BackToMenu()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+
+	public void Credits()
+	{
+		SceneManager.LoadScene("Credits");
+	}
+
+	public void Info()
+	{
+		SceneManager.LoadScene("Info");
+	}
+
+	public void Play()
+	{
+		SceneManager.LoadScene("Game");
+	}
+}
---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+
+	public float time = 2f;
+	public string levelToLoad = "Menu";
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("DisplayScene");
+	}
+
+	IEnumerator DisplayScene(){
+		yield return new WaitForSeconds(time);
+		Application.LoadLevel (levelToLoad);
+	}
+}
